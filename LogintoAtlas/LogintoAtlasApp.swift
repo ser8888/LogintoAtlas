473f08188636b1782e487d497299da3cf30df0b4
Sergejs Tiselskis <slokas85@gmail.con>
2022-11-01 18:01:32 +0200

    Initial Commit

---
+++ LogintoAtlas/LogintoAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogintoAtlasApp.swift
+//  LogintoAtlas
+//
+//  Created by John Doe on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LogintoAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
